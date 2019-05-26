a81b819a7d2c8fb9d86c99594da73e60170e3693
Hashir Zahir <hashirsahad@gmail.com>
2019-05-26 16:56:28 +0800

    Generate annotations index file

--- web_annotation_tool/inc/list_files.php
+++ web_annotation_tool/inc/list_files.php
@@ -1,9 +1,18 @@
 <?php
 include "configuration.php";
-$files = scandir ($IMAGE_ROOT_DIR); 
-$fp = fopen('file_list.txt', 'w');
-for($i = 0; $i < count($files); ++$i){
-    echo "". $files[$i] . "\n";
-    fwrite($fp, $files[$i] . "\n");
+// create index of images in images folder
+function create_index($dir, $index_name) 
+{
+    $files = scandir ($dir); 
+    $fp = fopen($index_name, 'w');
+    for($i = 0; $i < count($files); ++$i){
+        echo "". $files[$i] . "\n";
+        fwrite($fp, $files[$i] . "\n");
+    }
+    fclose($fp);
 }
-fclose($fp);
+
+create_index($IMAGE_ROOT_DIR, 'file_list.txt');
+create_index($ANNOTATIONS_DIR, 'annotation_list.txt');
+
+?>
